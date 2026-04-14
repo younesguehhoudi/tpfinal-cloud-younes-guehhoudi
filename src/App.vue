5acb34cb55771537bed2cf20e2d40c8b0164ab85
younesguehhoudi <younesguehhoudi@github.com>
2026-04-14 13:08:56 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+const buildId = import.meta.env.VITE_BUILD_ID ?? 'local'
+const today = new Date().toLocaleDateString('fr-FR')
+</script>
+<template>
+<header>
+<p>Nom : Younes Guehhoudi</p>
+<p>Date : {{ today }}</p>
+<p>Build : {{ buildId }}</p>
+</header>
+</template>
