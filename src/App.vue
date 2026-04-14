6ad202a275460a01d9bb49c4afc8c3aee98ee5b2
younesguehhoudi <younesguehhoudi@github.com>
2026-04-14 13:46:43 +0200

    Update .dockerignore and modify name in App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,10 @@
 const buildId = import.meta.env.VITE_BUILD_ID ?? 'local'
 const today = new Date().toLocaleDateString('fr-FR')
 </script>
+
 <template>
 <header>
-<p>Nom : Younes Guehhoudi</p>
+<p>Nom : Younés Guehhoudi L3-SDN</p>
 <p>Date : {{ today }}</p>
 <p>Build : {{ buildId }}</p>
 </header>
